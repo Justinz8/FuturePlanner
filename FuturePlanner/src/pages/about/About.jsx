4a12d4a43c8b4a880e35d21752680ab85e3e6366
Justinz8 <Justinzhang9001@gmail.com>
2024-06-20 12:33:51 -0400

    Deleted read me and changes to about page
    fixed grammar for about page and deleted read me file in front end folder

--- FuturePlanner/src/pages/about/About.jsx
+++ FuturePlanner/src/pages/about/About.jsx
@@ -5,10 +5,10 @@ export default function About() {
         <div className='About-Wrapper'>
             <div className='About-Body'>
                 <h1>About</h1>
-                <p>The UofT and UW future planner is a student-built side project that aims to help solve the common struggles from trying to plan an undergraduate career.</p>
-                <p>Allowing students to quickly plan out when and what courses they wish to take in the future; The goal for this website is to help students from both UTSC and UW plan ahead further than just picking courses for the next term and help create a strong plan for students to stay organised on their undergraduate career.</p>
-                <p>If you have any questions or concerns please send the team an <a href='mailto:uoftuwfutureplanner@gmail.com'>email</a>.</p>
-                <p>Thanks for check out the future planner!</p>
+                <p>The UofT and UW Future Planner is a student-built side project designed to address the common challenges of planning an undergraduate career.</p>
+                <p>The goal of this website is to assist students from both UofT and UW in planning their future courses efficiently. It aims to help students create a comprehensive academic plan beyond just selecting courses for the next term, ensuring they stay organized throughout their undergraduate careers.</p>
+                <p>If you have any questions or concerns please send the team at <a href='mailto:uoftuwfutureplanner@gmail.com'>uoftuwfutureplanner@gmail.com</a>.</p>
+                <p>Thanks for checking out the future planner!</p>
             </div>
         </div>
         
